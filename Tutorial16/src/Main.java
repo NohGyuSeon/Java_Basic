52d81f8f34534f0f01d707cfe584a6c22558220b
NGS <kusun1020@gmail.com>
2022-06-19 17:00:59 +0900

    Add files via upload

---
+++ Tutorial16/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Student student1 = new Student("노규선", 23 ,168, 58, "20182337", 2, 4.5);
+		Student student2 = new Student("노규딱", 20 ,168, 52, "20182337", 1, 4.5);
+		student1.show();
+		student2.show();
+		
+	}
+	
+}
---
+++ Tutorial20/src/Main.java
@@ -0,0 +1,34 @@
+
+public class Main implements Dog, Cat {
+
+	public static void main(String[] args) {
+		
+		Main main = new Main();
+		main.crying();
+		main.one();
+		main.two();
+		
+	}
+
+	@Override
+	public void crying() {
+	
+		System.out.println("¿ù! ¿ù!");
+		
+	}
+
+	@Override
+	public void two() {
+		
+		System.out.println("Two!");
+		
+	}
+
+	@Override
+	public void one() {
+		
+		System.out.println("One!");
+	
+	}
+	
+}
